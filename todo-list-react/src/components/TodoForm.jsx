22ab138f6f9bb49dd9a46fd813f8aecf36f8c699
andrew malcher <andrew.malcher.r@gmail.com>
2023-10-27 17:57:58 -0400

    commit inicial 0.2

--- todo-list-react/src/components/TodoForm.jsx
+++ todo-list-react/src/components/TodoForm.jsx
@@ -9,21 +9,20 @@ const TodoForm = (addTodo) => {
         e.preventDefault();
         if(!value || !category) return;
         addTodo(value,category);
-        setValue("")
-        setCategory("")
+        setValue("");
+        setCategory("");
         //limpar os campos
-        
     }
 
     return (
         <div className='todo-form'>
             <h2>Criar Tarefa:</h2>
             {/* // adiciona a função do handleSubmit ao enviar o formulario */}
-            <form onSubmit={handleSubmit}> 
-                <input 
+            <form onSubmit={handleSubmit}>
+                <input
                 value={value}
-                type="text" 
-                placeholder='Digite o Título' 
+                type="text"
+                placeholder='Digite o Título'
                 onChange={(e) => setValue(e.target.value)} />
                 <select
                 onChange={(e) => setCategory(e.target.value)}
