c2066aab9757159ce547818faa47e5d7bdc56196
andrew malcher <andrew.malcher.r@gmail.com>
2023-10-27 21:48:55 -0400

    commit inicial 0.3

--- todo-list-react/src/components/Todo.jsx
+++ todo-list-react/src/components/Todo.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Todo = ({todo, removeTodo, completeTodo }) => {
     return (
         <div className='todo' style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
--- todo-list-react/src/components/TodoForm.jsx
+++ todo-list-react/src/components/TodoForm.jsx
@@ -1,41 +1,38 @@
-import { useState } from "react"
+import React, { useState } from "react";
 
-
-const TodoForm = (addTodo) => {
+const TodoForm = ({ addTodo }) => {
     const [value, setValue] = useState("");
     const [category, setCategory] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(!value || !category) return;
-        addTodo(value,category);
+        if (!value || !category) return;
+        addTodo(value, category);
         setValue("");
         setCategory("");
-        //limpar os campos
-    }
+        // Limpar os campos
+    };
 
     return (
-        <div className='todo-form'>
+        <div className="todo-form">
             <h2>Criar Tarefa:</h2>
-            {/* // adiciona a função do handleSubmit ao enviar o formulario */}
             <form onSubmit={handleSubmit}>
                 <input
-                value={value}
-                type="text"
-                placeholder='Digite o Título'
-                onChange={(e) => setValue(e.target.value)} />
-                <select
-                onChange={(e) => setCategory(e.target.value)}
-                value={value}>
-                    <option value="" >Selecione a categoria</option>
-                    <option value="Trabalho" >Trabalho</option>
-                    <option value="Pessoal" >Pessoal</option>
-                    <option value="Estudos" >Estudos</option>
+                    value={value}
+                    type="text"
+                    placeholder="Digite o Título"
+                    onChange={(e) => setValue(e.target.value)}
+                />
+                <select onChange={(e) => setCategory(e.target.value)} value={category}>
+                    <option value="">Selecione a categoria</option>
+                    <option value="Trabalho">Trabalho</option>
+                    <option value="Pessoal">Pessoal</option>
+                    <option value="Estudos">Estudos</option>
                 </select>
-                <button type='submit'>CRIAR</button>
+                <button type="submit">CRIAR</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default TodoForm
+export default TodoForm;
