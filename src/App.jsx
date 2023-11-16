23967f36e3f880cff0130315c042de15be784e44
AndrewMalcher <andrew.malcher.r@gmail.com>
2023-11-16 09:07:44 -0400

    Modificacao do nome todo para Todo, no App.jsx. Fix of issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Todo from './components/todo'
+import Todo from './components/Todo'
 import TodoForm from './components/TodoForm'
 import Search from './components/Search'
 import Filter from './components/Filter'
